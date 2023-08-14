fe0e19164c4d27dbffb4e3020666ae94ec1ca419
Stephaniekioks <kiokosteff.ks@gmail.com>
2023-08-14 14:16:28 -0700

    8-multiple_returns.py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-def multiple_returns(sentese):
+# 8-multiple_returns.py
+
+def multiple_returns(sentence):
     """returns the length of a string and its first character."""
     if sentense == '':
         return (0, None)
-    return (len(sentence), sentense[0])
+    return (len(sentence), sentence[0])
