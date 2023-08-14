27832b6020b1d31e27505fe4617e379dd34e9964
Stephaniekioks <kiokosteff.ks@gmail.com>
2023-08-14 14:05:28 -0700

    5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
+# 5-no_c.py
 
 def no_c(my_string):
     update_str = ''
     for i in my_string:
         if i != 'c' and i != 'C':
             update_str += i
-
             return (updated_str)
