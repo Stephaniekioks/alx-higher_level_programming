59dc4848769c8cfeee4832b6677b0426987ec8a6
Stephaniekioks <kiokosteff.ks@gmail.com>
2023-08-16 12:53:39 -0700

    1-calculation.py

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,6 +1,3 @@
-1-calculation.py
-
-
 #!/usr/bin/python3
 
 if __name__ == "__main__":
@@ -9,7 +6,6 @@ if __name__ == "__main__":
 
     a = 10
     b = 5
-
     print("{} + {} = {}".format(a, b, add(a, b)))
     print("{} - {} = {}".format(a, b, sub(a, b)))
     print("{} * {} = {}".format(a, b, mul(a, b)))
